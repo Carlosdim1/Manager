35b831eb334e988a0bedb3fa167d42ec1f8e6046
Carlosdim <carlosdim_@hotmail.com>
2022-07-02 23:34:04 +0200

    first commit

---
+++ menu.py
@@ -0,0 +1,46 @@
+""" Menú del programa """
+
+import helpers
+import manager
+
+
+def loop():
+    while True:
+        helpers.clear()  # 'clear' para Linux y OS X
+
+        print("========================")
+        print("  BIENVENIDO AL GESTOR  ")
+        print("========================")
+        print("[1] Listar clientes     ")
+        print("[2] Mostrar cliente     ")
+        print("[3] Añadir cliente      ")
+        print("[4] Modificar cliente   ")
+        print("[5] Borrar cliente      ")
+        print("[6] Salir               ")
+        print("========================")
+
+        option = input("> ")
+
+        helpers.clear()  # 'clear' para Linux y OS X
+
+        if option == '1':
+            print("Listando los clientes...\n")
+            manager.show_all()
+        if option == '2':
+            print("Mostrando un cliente...\n")
+            manager.find()
+        if option == '3':
+            print("Añadir un cliente...\n")
+            manager.add()
+        if option == '4':
+            print("Modificando un cliente...\n")
+            if manager.edit():
+                print("Modificado correctamente")
+        if option == '5':
+            print("Borrando un cliente...\n")
+            manager.delete()
+        if option == '6':
+            print("Saliendo...\n")
+            break
+
+        input("\nPresiona ENTER para continuar...")
